53cf4d1e1dd51b737eebbec0ab410de919778086
unknown <dasha.nebogina@mail.ru>
2025-01-30 16:21:27 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const k = 0.693 / HALF_LIFE_PERIOD;
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +18,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = Number(sampleActivity);
+  if (!activity || typeof sampleActivity !== "string" || activity <= 0 || activity > 15) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
 }
 
 module.exports = {
